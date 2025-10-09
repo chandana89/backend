392eb452e8d33f9061e1e3219021b76c5e708622
Naga Chandana Mudusu <Chandana.Mudusu@rewardpay.com.au>
2025-10-09 14:55:52 +1100

    extract BOs using file url

--- src/modules/chatgpt/chatgpt.controller.ts
+++ src/modules/chatgpt/chatgpt.controller.ts
@@ -1,15 +1,39 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, Req, Get } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, Req, Get, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { ChatGPTService } from './chatgpt.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('chatgpt')
 export class ChatGPTController {
   constructor(private chatGPTService: ChatGPTService) {}
 
     @Get()
-    public async handleAddIndustry() {
+    public async generateResponse() {
 
         try {
-            const response = await this.chatGPTService.generateResponse('extract beneficial owners');
+            const response = await this.chatGPTService.GenerateResponse('extract beneficial owners');
+            return response;
+        } catch (e) {
+            console.log(`Error: ${e.message}`);
+        }
+    }
+
+    @Post()
+    @UseInterceptors(FileInterceptor('asic'))
+    public async uploadFile(@Req() req: Request, @Body() body: {asic?: any},@UploadedFile() asic: any,) {
+
+        try {
+            const response = await this.chatGPTService.UploadFile(asic);
+            return response;
+        } catch (e) {
+            console.log(`Error: ${e.message}`);
+        }
+    }
+
+    @Post('url')
+    public async ExtractBOs(@Req() req: Request, @Body() body: {fileURL: string}) {
+
+        try {
+            const response = await this.chatGPTService.ExtractBeneficialOwnerUsingURL(body.fileURL);
             return response;
         } catch (e) {
             console.log(`Error: ${e.message}`);
--- src/modules/chatgpt/chatgpt.service.ts
+++ src/modules/chatgpt/chatgpt.service.ts
@@ -1,5 +1,7 @@
 import { Injectable} from '@nestjs/common';
 import axios from 'axios';
+import FormData from 'form-data';
+
 
 @Injectable()
 export class ChatGPTService {
@@ -8,16 +10,13 @@ export class ChatGPTService {
 
   constructor() {
     this.apiKey = process.env.OPEN_API_KEY || '';
-    this.apiUrl = 'https://api.openai.com/v1/conversations';
+    this.apiUrl = 'https://api.openai.com/v1/responses';
   }
 
-  public async generateResponse(prompt: string) {
+  public async GenerateResponse(prompt: string) {
     const data = {
-        prompt: prompt,
-        max_tokens: 150,
-        n: 1,
-        stop: null,
-        temperature: 1,
+        model: "gpt-4.1",
+        input: prompt
     };
 
     const headers = {
@@ -26,5 +25,78 @@ export class ChatGPTService {
     };
 
     return axios.post(this.apiUrl, data, { headers: headers });
+  }
+
+  public async UploadFile(asic: Express.Multer.File) {
+    const formData = new FormData();
+    formData.append('file', asic.buffer, asic.originalname);
+    formData.append('purpose', 'assistants');
+
+    const headers = {
+      ...formData.getHeaders(),
+      Authorization: `Bearer ${this.apiKey}`,
+    };
+
+    const response = await axios.post('https://api.openai.com/v1/files', formData, { headers });
+    const beneficialOwners = await this.ExtractBeneficialOwner(response.data.id)
+    return beneficialOwners; 
+  }
+
+  public async ExtractBeneficialOwner(fileId: String) {
+   const data = {
+        model: "gpt-5",
+        input: [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "input_file",
+                        "file_id": fileId
+                    },
+                    {
+                        "type": "input_text",
+                        "text": "Extract beneficial owners"
+                    }
+                ]
+            }
+        ]
+    };
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.apiKey}`,
+    };
+
+    const res = await axios.post('https://api.openai.com/v1/responses', data, { headers: headers });
+    return res.data;
+  }
+
+  public async ExtractBeneficialOwnerUsingURL(fileURL: String) {
+   const data = {
+        model: "gpt-5",
+        input: [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "input_file",
+                        "file_url": fileURL
+                    },
+                    {
+                        "type": "input_text",
+                        "text": "Extract beneficial owners"
+                    }
+                ]
+            }
+        ]
+    };
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.apiKey}`,
+    };
+
+    const res = await axios.post('https://api.openai.com/v1/responses', data, { headers: headers });
+    return res.data;
   }
 }
